f89297891a1374923a93fe470583f96fc8df5d8b
aramsing <aramsin2@hotmail.com>
2025-05-26 18:20:54 -0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import About from './components/About'
+import Home from './components/Home'
 
 function App() {
   return (
-    <>
-      <h1>Hello, welcome to my website!</h1>
-      <img src='https://media.licdn.com/dms/image/v2/D5603AQGb1vGg-lofyA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1707169444707?e=1753920000&v=beta&t=W1w8yJW_9CcWZ8db-7k5_32ee8TqxFks6Y4kGtIinBY' alt='Arjun Ramsinghani' />
-      <p>My name is Arjun Ramsinghani and I am a Software Engineer.</p>
+    <>     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
   )
 }
